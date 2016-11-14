992dd61ac485a5536854512eb5bcf2ade5bbef59
Igor Chorazewicz <chorig9@gmail.com>
2016-11-14 19:21:06 +0100

    Contours finding

--- ObjectTracking/main.cpp
+++ ObjectTracking/main.cpp
@@ -17,6 +17,10 @@ void detectAndDisplay(Mat frame);
 String window_name = "Object Tracking";
 String edges_window = "Edges";
 
+int thresh = 100;
+int max_thresh = 255;
+RNG rng(12345);
+
 /** @function main */
 int main(void)
 {
@@ -24,6 +28,9 @@ int main(void)
 	Mat frame;
 
 	if (!capture.isOpened()) { printf("--(!)Error opening video capture\n"); return -1; }
+
+	namedWindow("Source");
+	createTrackbar("Threshold:", "Source", &thresh, max_thresh);
 
 	while (capture.read(frame))
 	{
@@ -33,7 +40,7 @@ int main(void)
 			break;
 		}
 
-		//-- 3. Apply the classifier to the frame
+		cvtColor(frame, frame, COLOR_BGR2GRAY);
 		detectAndDisplay(frame);
 
 		int c = waitKey(20);
@@ -42,12 +49,42 @@ int main(void)
 	return 0;
 }
 
-/** @function detectAndDisplay */
 void detectAndDisplay(Mat frame)
 {
-	Mat edges;
-	Canny(frame, edges, 150, 450);
+	Mat threshold_output;
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
 
-	imshow(window_name, frame);
-	imshow(edges_window, edges);
+	/// Detect edges using Threshold
+	threshold(frame, threshold_output, thresh, 255, THRESH_BINARY);
+	/// Find contours
+	findContours(threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+	/// Approximate contours to polygons + get bounding rects and circles
+	vector<vector<Point> > contours_poly(contours.size());
+	vector<Rect> boundRect(contours.size());
+	vector<Point2f>center(contours.size());
+	vector<float>radius(contours.size());
+
+	for (int i = 0; i < contours.size(); i++)
+	{
+		approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
+		boundRect[i] = boundingRect(Mat(contours_poly[i]));
+		minEnclosingCircle((Mat)contours_poly[i], center[i], radius[i]);
+	}
+
+
+	/// Draw polygonal contour + bonding rects + circles
+	Mat drawing = Mat::zeros(threshold_output.size(), CV_8UC3);
+	for (int i = 0; i< contours.size(); i++)
+	{
+		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+		drawContours(drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+		rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
+		circle(drawing, center[i], (int)radius[i], color, 2, 8, 0);
+	}
+
+	/// Show in a window
+	namedWindow("Contours", WINDOW_AUTOSIZE);
+	imshow("Contours", drawing);
 }
