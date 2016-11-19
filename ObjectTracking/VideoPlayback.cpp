0eff065bd29dd8084878459298f56b757d16610d
Igor Chorazewicz <chorig9@gmail.com>
2016-11-19 11:57:15 +0100

    video playing in thread

--- ObjectTracking/VideoPlayback.cpp
+++ ObjectTracking/VideoPlayback.cpp
@@ -1,31 +1,79 @@
 #include "VideoPlayback.h"
+#include <opencv2/imgproc.hpp>
 
+extern void detectAndDisplay(cv::Mat);
 
 VideoPlayback::VideoPlayback(cv::String filename, cv::String& win_name)
 {
 	capture.open(filename);
-	if (!capture.isOpened()) {
-		ready = false; 
-		return;
-	}
+	if (!capture.isOpened())
+		throw std::runtime_error("--(!)Error opening video capture\n");
 
 	while (capture.grab())
 		max_frames++;
 
 	capture.set(cv::CAP_PROP_POS_FRAMES, 0);
 	window_name = win_name;
-	ready = true;
+	running = true;
+
+	video_thread = std::thread(&VideoPlayback::play_video, this);
 }
 
-bool VideoPlayback::isReady()
+void VideoPlayback::play()
 {
-	return ready;
+	playback_state = true;
 }
 
-void VideoPlayback::skipToCurrent()
+void VideoPlayback::pause()
+{
+	playback_state = false;
+}
+
+void VideoPlayback::stop()
+{
+	running = false;
+	playback_state = false;
+}
+
+bool VideoPlayback::is_playing() const
+{
+	return running && playback_state;
+}
+
+VideoPlayback::~VideoPlayback()
+{
+	video_thread.join();
+}
+
+void VideoPlayback::skip_to_current_frame()
 {
 	cv::Mat image;
+	mutex.lock();
 	capture.set(cv::CAP_PROP_POS_FRAMES, current_frame);
 	capture.read(image);
+	cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
+	detectAndDisplay(image);
 	cv::imshow(window_name, image);
+	mutex.unlock();
+}
+
+void VideoPlayback::play_video()
+{
+	cv::Mat frame;
+
+	while (running)
+	{
+		mutex.lock();
+		if (playback_state && capture.read(frame))
+		{
+			if (frame.empty())
+				throw std::runtime_error(" --(!) No captured frame -- Break!");
+
+			cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+			detectAndDisplay(frame);
+			cv::imshow(window_name, frame);
+		}
+		mutex.unlock();
+		cv::waitKey(20);
+	}
 }
--- ObjectTracking/VideoPlayback.h
+++ ObjectTracking/VideoPlayback.h
@@ -4,21 +4,33 @@
 
 #include <thread>
 #include <string>
+#include <mutex>
 
 class VideoPlayback
 {
 public:
-	int current_frame = 0;
 	int max_frames = 0;
+	int current_frame = 0;
 
 	VideoPlayback(cv::String filename, cv::String& win_name);
-	bool isReady();
-	void skipToCurrent();
+	~VideoPlayback();
+
+	void play();
+	void pause();
+	void stop();
+
+	bool is_playing() const;
+
+	void skip_to_current_frame();
 
 private:
 	cv::VideoCapture capture;
 	bool playback_state = false;
-	bool ready = false;
+	volatile bool running = false; // not sure if it should be volatile (used in thread)
 	std::string window_name;
+
+	std::thread video_thread;
+	std::mutex mutex;
+	void play_video();
 };
 
